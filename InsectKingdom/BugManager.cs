9a7fb54d963c273eb931af5f1df9b91f8d77f266
MarenSusanne <Marensut@getacademy.no>
2024-06-19 11:33:12 +0200

    Add project files.

---
+++ InsectKingdom/BugManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using InsectKingdom;
+
+public static class BugManager
+{
+    private static List<Bug> _bugList;
+	static BugManager()
+    {
+        _bugList = new List<Bug>()
+        {
+            new Spider(),
+            new Fly(),
+            new Mosquito(),
+            new Tick(),
+            new Wasp(),
+            new Butterfly()
+        };
+    }
+}
---
+++ InsectKingdom/Butterfly.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using InsectKingdom;
+
+public class Butterfly : Bug
+{
+	public Butterfly() : base(true, false, 6, "eats plants")
+    {
+	}
+}
